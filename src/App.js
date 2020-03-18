617ccbfb0629aa076884fddfe9c63f167a630dbc
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-18 14:38:30 -0600

    updated with shop page, collection preview and item component'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
-  const HatsPage = () => (
-    <div>
-      <h1> Hats Page</h1>
-    </div>
-  )
+ 
   return (
     <div>
     <Switch>
      <Route exact path='/' component={HomePage} />
-     <Route exact path='hats' component={HatsPage} />
+     <Route path='/shop' component={ShopPage} />
     </Switch>
-     <HomePage />
+     
     </div>
   );
 }
