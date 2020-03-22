7f4a995dba5447245eaf210a90f5f46b2eca6370
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-21 21:01:56 -0600

    added card icon and interactive cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,9 @@ import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/original.svg'
 import {auth} from  '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/' >
       <Logo className='logo' />
@@ -25,12 +26,16 @@ const Header = ({ currentUser }) => (
         </Link>
         }
         <CartIcon />
+      
     </div>
+    { hidden ? null : <CartDropdown />
+    }
   </div>
 )
 
-const mapStateToProps= (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps= ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
