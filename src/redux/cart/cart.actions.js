4246ce0944f5ce162ffa0fb8b1fd6ff4ac258a7d
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-21 21:56:55 -0600

    updated cart utils to group items in cart by quantity

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,4 +4,9 @@ const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 })
 
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+})
+
 export default toggleCartHidden
