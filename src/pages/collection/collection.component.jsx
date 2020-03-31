1fc5c285bffd09f8f4efd97b41a33f4267ba2308
Seth Wheeler <wheeler.seth@gmail.com>
2020-03-30 21:36:41 -0600

    set up initial route and component for collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import collectionItem from '../../components/collection-item/collection-item.component'
+
+import './collection.styles.scss'
+
+const CollectionPage = ({ match }) => {
+  
+  return(
+  <div className='category'>
+    <h2> Category</h2>
+  </div>)
+}
+
+export default CollectionPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component'
 
-const ShopPage = ({ collections }) => {
-
+const ShopPage = ({ match }) => { //match location and history are passed as props through route
+    console.log(match)
     return(
       <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`}component={CollectionsOverview} />
+        <Route path ={`${match.path}/:categoryID`} component={CollectionPage}/>
       </div>
     )
   }
